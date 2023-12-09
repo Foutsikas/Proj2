e36e32b3dbd6513fe9ba7b7604d1483cb7c65e5b
Christos Fotos <c.foutsikas@gmail.com>
2023-12-09 05:09:18 +0200

    Escalation & Reduction
    Added functionality to increase and decrease the size of Bob with the "M" and "N" keys respectively.

--- Assets/Scripts/MoveBob.cs
+++ Assets/Scripts/MoveBob.cs
@@ -5,11 +5,20 @@ public class MoveBob : MonoBehaviour
     public float[] speedGradients = { 1f, 2f, 5f, 8f, 10f }; // Speed gradients
     public int currentSpeedIndex = 2; // Initial speed level
     public Terrain terrain; // Assign the terrain in the Inspector
+    public float explosionThreshold = 15f; // Threshold for explosion
+    public float escalateMultiplier = 1.5f; // Multiplier for escalation
+    public float reductionMultiplier = 1f; // Multiplier for gradual reduction
+
+    private float explosionForce = 1000f; // Set the force of the explosion
+    private float maxEscalation = 10f; // Set the maximum escalation before explosion
+    private float minReduction = 0.2f; // Set the maximum escalation before explosion
 
     void Update()
     {
         AdjustSpeed();
         MoveCharacter();
+        EscalateBob();
+        ReduceBob();
     }
 
     void AdjustSpeed()
@@ -63,5 +72,45 @@ public class MoveBob : MonoBehaviour
 
             transform.position = new Vector3(transform.position.x, newY, transform.position.z);
         }
+    }
+
+    void EscalateBob()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            transform.localScale *= escalateMultiplier;
+        }
+        if (transform.localScale.x > maxEscalation)
+        {
+            Explode();
+        }
+    }
+
+    void ReduceBob()
+    {
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            // Gradually reduce the scale over time
+            transform.localScale *= reductionMultiplier;
+        }
+        if (transform.localScale.x < minReduction)
+        {
+            Explode();
+        }
+    }
+
+    void Explode()
+    {
+        // Perform explosion
+        Rigidbody rb = gameObject.GetComponent<Rigidbody>();
+        if (rb == null)
+        {
+            rb = gameObject.AddComponent<Rigidbody>();
+        }
+
+        rb.AddExplosionForce(explosionForce, transform.position, 5f);
+
+        // Optionally, you can destroy the object after the explosion
+        Destroy(gameObject);
     }
 }
